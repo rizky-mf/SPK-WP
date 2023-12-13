3be93b60b1c783ffe185b64676113d4d3dbb89b8
Rizky Maolana Firdaus <rizkymaulanafrds@gmail.com>
2023-12-13 13:47:44 +0700

    feat: update dataset

--- src/data/dataset.ts
+++ src/data/dataset.ts
@@ -1,24 +1,22 @@
 const dataset = [
-    {
-      no: 1,
-      name: "Rizky",
-      competition: 1,
-      academic: 77,
-      skill: 78,
-      absence: 1,
-      organization: 1,
-    },
-    {
-        no: 1,
-        name: "Beni",
-        competition: 1,
-        academic: 79,
-        skill: 80,
-        absence: 1,
-        organization: 1,
-      },
-    
-  ];
-  
-  export default dataset;
-  
+  {
+    no: 1,
+    name: "Rizky",
+    competition: 1,
+    academic: 77,
+    skill: 78,
+    absence: 1,
+    organization: 1,
+  },
+  {
+    no: 2,
+    name: "Beni",
+    competition: 1,
+    academic: 79,
+    skill: 80,
+    absence: 1,
+    organization: 1,
+  },
+];
+
+export default dataset;
