0b2f15890be4db1f2de4a90de5a2ac470ad2d0ac
Rizky Maolana Firdaus <rizkymaulanafrds@gmail.com>
2023-12-22 02:07:28 +0700

    feat: update sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,6 @@
 import { Box, Divider, Heading, HStack, Text, VStack } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/react";
 import { BiData } from "react-icons/bi";
-// import { FaGithub } from "react-icons/fa";
 import { GiWeightCrush } from "react-icons/gi";
 import { IconType } from "react-icons/lib";
 import { useNavigate } from "react-location";
@@ -46,21 +45,6 @@ const Sidebar = () => {
       <Heading as="h3" fontSize="3xl" mb="2">
         Aplikasi SPK - Pemilihan Siswa Berprestasi - WP
       </Heading>
-      {/* <Button
-        leftIcon={<FaGithub />}
-        bg="white"
-        color="blue.400"
-        boxShadow="lg"
-        width="full"
-        justifyContent="start"
-      >
-        <a
-          target="_blank"
-          href="https://github.com/rizky-mf"
-        >
-          Source Code
-        </a>
-      </Button> */}
       <Divider my="4" />
       <Text textTransform="uppercase" opacity="75" mb="3" fontWeight="bold">
         Main
